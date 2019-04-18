13019f24d44e9032de2d191f6c720395682d9001
Michael <lordrhodesy@gmail.com>
2019-04-17 21:48:47 -0600

    Remove Employees

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
     super(props);
 
     this.handleAddEmployee = this.handleAddEmployee.bind(this);
+    this.handleRemoveEmployee = this.handleRemoveEmployee.bind(this);
   }
 
   handleAddEmployee(e) {
@@ -19,6 +20,20 @@ class App extends Component {
     Scheduler.employees.push(new Employee(name));
     document.getElementById("dropdown").innerHTML = name;
     Scheduler.loadEmployeeSchedule();
+    this.setState({});
+  }
+
+  handleRemoveEmployee(e) {
+    Scheduler.employees.splice(Scheduler.selectedIndex,1);
+    if (Scheduler.employees.length > 0) {
+      Scheduler.selectedIndex = 0;
+      document.getElementById("dropdown").innerHTML = Scheduler.employees[0].name;
+      Scheduler.loadEmployeeSchedule();
+    } else {
+      Scheduler.selectedIndex = -1;
+      document.getElementById("dropdown").innerHTML = "";
+      Button.pressClear(e);
+    }
     this.setState({});
   }
 
@@ -45,9 +60,6 @@ class App extends Component {
           break;
         case "Set Disabled":
           Button.pressSetDisabled(e);
-          break;
-        case "Add Employee":
-          Button.pressAddEmployee(e);
           break;
         case "Clear Schedule":
           Button.pressClear(e);
@@ -89,22 +101,29 @@ class App extends Component {
           <div className="right-panel">
             <div className="buttons">
               <h2>Schedule:</h2>
-              <Button id="workScheduleButton" className="disabled" name="Work Schedule" handler={this.handleClickButton}/>
+              <Button id="workScheduleButton" className="disabled" name="Work Schedule"
+                      handler={this.handleClickButton}/>
               <br/>
-              <Button id="employeeSchedulesButton" className="enabled" name="Employee Schedules" handler={this.handleClickButton}/>
+              <Button id="employeeSchedulesButton" className="enabled" name="Employee Schedules"
+                      handler={this.handleClickButton}/>
               <div id="modeDiv">
                 <h2 id="modeLabel">Mode:</h2>
-                <Button id="setUnavailableButton" className="disabled" name="Set Unavailable" handler={this.handleClickButton}/>
+                <Button id="setUnavailableButton" className="disabled" name="Set Unavailable"
+                        handler={this.handleClickButton}/>
                 <br/>
-                <Button id="setScheduledButton" className="disabled" name="Set Scheduled" handler={this.handleClickButton}/>
+                <Button id="setScheduledButton" className="disabled" name="Set Scheduled"
+                        handler={this.handleClickButton}/>
                 <br/>
-                <Button id="setDisabledButton" className="disabled" name="Set Disabled" handler={this.handleClickButton}/>
+                <Button id="setDisabledButton" className="disabled" name="Set Disabled"
+                        handler={this.handleClickButton}/>
               </div>
               <div id="staffDiv">
                 <h2 id="StaffLabel">Staff:</h2>
-                <Button id="addEmployeeButton" className="enabled" name="Add Employee" handler={this.handleAddEmployee}/>
+                <Button id="addEmployeeButton" className="enabled" name="Add Employee"
+                        handler={this.handleAddEmployee}/>
                 <br/>
-                <Button id="removeEmployeeButton" className="enabled" name="Remove Employee"/>
+                <Button id="removeEmployeeButton" className="enabled" name="Remove Employee"
+                        handler={this.handleRemoveEmployee}/>
               </div>
             </div>
           </div>
